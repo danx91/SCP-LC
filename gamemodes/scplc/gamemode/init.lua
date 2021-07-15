fe239a487832ef4b12862a93a82c805aa7574f53
vytkin_vs <vyatkin_vs@taximaxim.ru>
2021-07-15 18:21:43 +0500

    Version checker

--- gamemodes/scplc/gamemode/init.lua
+++ gamemodes/scplc/gamemode/init.lua
@@ -2,3 +2,17 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
+
+timer.Simple(0,function()
+
+    http.Fetch("https://raw.githubusercontent.com/danx91/SCP-LC/master/gamemodes/scplc/gamemode/modules/sh_module.lua",function(data)
+
+        if string.find( data, [[VERSION = "BETA 0.7.2"]] ) then
+            print("[SCP:LC] You using lasted version!")
+        else
+            print("[SCP:LC] Your version is outdated update your gamemode in workshop or github (https://github.com/danx91/SCP-LC)")
+        end
+    
+    end)
+
+end)
